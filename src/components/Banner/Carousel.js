1c3433a15900596511ba7b35f34e01050f165420
Tatiana Cheah <tatiana.cheah@gmail.com>
2022-02-21 11:57:27 +0800

    Responsiveness to Banner

--- src/components/Banner/Carousel.js
+++ src/components/Banner/Carousel.js
@@ -1,4 +1,3 @@
-import { makeStyles } from "@material-ui/core";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import AliceCarousel from "react-alice-carousel";
@@ -6,6 +5,7 @@ import { Link } from "react-router-dom";
 import { TrendingCoins } from "../../config/api";
 import { CryptoState } from "../../CryptoContext";
 import { numberWithCommas } from "../CoinsTable";
+import { useStyles } from "./trendStyle.js";
 
 const Carousel = () => {
   const [trending, setTrending] = useState([]);
@@ -22,45 +22,6 @@ const Carousel = () => {
     fetchTrendingCoins();
   }, [currency]);
 
-  const useStyles = makeStyles((theme) => ({
-    carousel: {
-      height: "50%",
-      maxWidth: "100%",
-      minWidth: "30%",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      flexWrap: "wrap",
-    },
-    carouselItem: {
-      display: "flex",
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "center",
-      cursor: "pointer",
-    },
-    coinImage: {
-      width: "40%",
-      display: "flex",
-      flexDirection: "column",
-      textTransform: "uppercase",
-      color: "white",
-      alignItems: "center",
-    },
-    coinDetails: {
-      width: "60%",
-      display: "flex",
-      flexDirection: "column",
-      textTransform: "uppercase",
-      color: "white",
-    },
-    coinInfo: {
-      display: "flex",
-      flexDirection: "row",
-      justifyContent: "flex-start",
-    },
-  }));
-
   const classes = useStyles();
 
   const items = trending.map((coin) => {
@@ -69,45 +30,37 @@ const Carousel = () => {
     return (
       <Link className={classes.carouselItem} to={`/coins/${coin.id}`}>
         <div className={classes.coinImage}>
-          <img
-            src={coin?.image}
-            alt={coin.name}
-            height="200"
-            style={{ marginBottom: 10 }}
-          />
+          <img src={coin?.image} alt={coin.name} />
         </div>
         <div className={classes.coinDetails}>
-          <span
-            style={{
-              fontSize: 66,
-              fontWeight: 500,
-              fontFamily: "Antonio",
-              color: "rgb(156, 204, 149)",
-            }}
-          >
-            {coin?.name}
-          </span>
-          <div className={classes.coinInfo}>
+          <span className={classes.coinTitle}>{coin?.name}</span>
+          <div className={classes.coinHighlight}>
+            <span className={classes.coinPrice}>
+              {symbol} {numberWithCommas(coin?.current_price.toFixed(2))}
+            </span>
+            &nbsp;
             <span
+              className={classes.coinChange}
               style={{
                 color: profit > 0 ? "rgb(48, 231, 57)" : "red",
-                fontSize: 30,
-                fontWeight: 500,
               }}
             >
               {profit && "+"}
               {coin?.price_change_percentage_24h?.toFixed(2)}%
             </span>
+          </div>
+          <div className={classes.coinInfo}>
+            <span className={classes.volTitle}>24 Hour Volume</span>
             &nbsp;
-            <span
-              style={{
-                fontSize: 30,
-                fontWeight: 500,
-                flexGrow: 2,
-                justifyContent: "center",
-              }}
-            >
-              {symbol} {numberWithCommas(coin?.current_price.toFixed(2))}
+            <span className={classes.coinVol}>
+              {symbol} {numberWithCommas(coin?.total_volume.toFixed(2))}
+            </span>
+          </div>
+          <div className={classes.coinInfo}>
+            <span className={classes.capitalTitle}>MKT CAPITAL</span>
+            &nbsp;
+            <span className={classes.capital}>
+              {symbol} {numberWithCommas(coin?.market_cap.toFixed(2))}
             </span>
           </div>
         </div>
@@ -117,9 +70,6 @@ const Carousel = () => {
 
   const responsive = {
     0: {
-      items: 2,
-    },
-    512: {
       items: 1,
     },
   };
