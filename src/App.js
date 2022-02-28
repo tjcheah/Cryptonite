775d86d4132103e9b20f12726e35bf26f1459673
Haley Gan <jiahuan19971105@gmail.com>
2022-02-28 02:18:30 +0000

    1st monday code commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
-import Header from './components/Header'
-import Homepage from './Pages/Homepage'
-import Coinpage from './Pages/Coinpage'
-import { makeStyles } from '@material-ui/core'
-import Alert from './components/Alert'
-import Footer from './components/Footer'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Homepage from "./Pages/Homepage";
+import Coinpage from "./Pages/Coinpage";
+import { makeStyles } from "@material-ui/core";
+import Alert from "./components/Alert";
+import Footer from "./components/Footer";
+import AboutUs from "./Pages/AboutUs";
 
 function App() {
   const useStyles = makeStyles(() => ({
     App: {
-      backgroundColor: '#f2f2f2',
-      color: 'white',
-      minHeight: '100vh',
+      backgroundColor: "#f2f2f2",
+      color: "white",
+      minHeight: "100vh",
     },
-  }))
+  }));
 
-  const classes = useStyles()
+  const classes = useStyles();
 
   return (
     <>
@@ -27,13 +28,14 @@ function App() {
           <Routes>
             <Route path="/" element={<Homepage />} exact />
             <Route path="/coins/:id" element={<Coinpage />} />
+            <Route path="/about" element={<AboutUs />} />
           </Routes>
           <Footer />
         </div>
         <Alert />
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/AboutUs/LogoBanner.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+import logo from "../Cryptonite Logo.png";
+import logoTitle from "../Cryptonite Title Cropped.png";
+
+const useStyles = makeStyles((theme) => ({
+  logoContainer: {
+    backgroundColor: "#dcdddb",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingRight: 0,
+    paddingLeft: 0,
+    marginLeft: 0,
+    marginRight: 0,
+    width: "100%",
+    height: 120,
+    boxShadow: "0px -2px 4px 1px #999",
+    [theme.breakpoints.down("sm")]: {
+      height: 60,
+      justifyContent: "flex-end",
+      alignItems: "flex-start",
+    },
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    [theme.breakpoints.down("sm")]: {
+      width: 150,
+      height: 150,
+      marginRight:50,
+    },
+  },
+  logoTitle: {
+    width: 360,
+    height: 90,
+    marginLeft: 20,
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
+    },
+  },
+}));
+
+const LogoBanner = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.logoContainer}>
+      <img src={logo} alt="logo" className={classes.logo} />
+      <img src={logoTitle} alt="logoTitle" className={classes.logoTitle} />
+    </div>
+  );
+};
+
+export default LogoBanner;
