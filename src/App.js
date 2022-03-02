d16b99eaf52ea65d1ee96eea16562c6e0731b159
Haley Gan <jiahuan19971105@gmail.com>
2022-03-02 00:58:30 +0000

    help page css

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import { makeStyles } from "@material-ui/core";
 import Alert from "./components/Alert";
 import Footer from "./components/Footer";
 import AboutUs from "./Pages/AboutUs";
+import Market from "./Pages/Forex";
 import Topo from "./media/topo.jpg";
+import Forex from "./Pages/Forex";
+import Help from "./Pages/Help";
 
 function App() {
   const useStyles = makeStyles(() => ({
@@ -52,6 +55,8 @@ function App() {
             <Route path="/" element={<Homepage />} exact />
             <Route path="/coins/:id" element={<Coinpage />} />
             <Route path="/about" element={<AboutUs />} />
+            <Route path="/market" element={<Forex />} />
+            <Route path="/help" element={<Help />} />
           </Routes>
           <Footer />
         </div>
