b370a4f24dd466963b8776ca3cecf16161a50c33
Amirul Hazwan <amirulhazwan1997@gmail.com>
2022-03-08 16:42:11 +0800

    Landing Page Design Update

--- src/components/LandingPage/containers/header/Header.jsx
+++ src/components/LandingPage/containers/header/Header.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./header.css";
 import logo from "../../../../media/Cryptonite Title (Silver).png";
+
 const Header = () => (
-  <div className="crypto__header section__padding" id="home">
+  <div className="crypto__header" id="home">
     <div className="crypto__header-content">
       <div className="crypto__header-image">
         <img src={logo} alt="logo" />
       </div>
-      <h1>The Crypto Market's #1 weakness</h1>
+
+      <h1>The Crypto Market's Number 1 weakness</h1>
+
       <p>
         The all-in-one overview of the cryptocurrency and forex market for both
         new and experienced traders alike.
