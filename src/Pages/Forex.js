eb694e0d187cb4b0641633a948f272850d613184
Haley Gan <jiahuan19971105@gmail.com>
2022-03-09 08:46:55 +0000

    update css and animations

--- src/Pages/Forex.js
+++ src/Pages/Forex.js
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
-import { Line } from 'react-chartjs-2'
+import { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
 import {
   CircularProgress,
   createTheme,
@@ -11,15 +11,17 @@ import "chart.js/auto";
 import { Typography } from "@material-ui/core";
 import { useSearchParams } from "react-router-dom";
 import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
+import { SlideDown } from "react-slidedown";
+import "react-slidedown/lib/slidedown.css";
 
 const GbpUsd = ({ coin }) => {
-  const [historicData, setHistoricData] = useState()
-  const [flag, setflag] = useState(false)
+  const [historicData, setHistoricData] = useState();
+  const [flag, setflag] = useState(false);
 
-  const [searchParams, setSearchParams] = useSearchParams()
-  const forex = searchParams.get('forex')
-    ? searchParams.get('forex')
-    : 'frxGBPUSD'
+  const [searchParams, setSearchParams] = useSearchParams();
+  const forex = searchParams.get("forex")
+    ? searchParams.get("forex")
+    : "frxGBPUSD";
 
   const useStyles = makeStyles((theme) => ({
     mainContainer: {
@@ -31,12 +33,12 @@ const GbpUsd = ({ coin }) => {
       margin: 0,
     },
     contContainer: {
-      display: 'flex',
-      flexDirection: 'column',
-      width: '60%',
+      display: "flex",
+      flexDirection: "column",
+      width: "60%",
       margin: 50,
-      [theme.breakpoints.down('lg')]: {
-        width: '80%',
+      [theme.breakpoints.down("lg")]: {
+        width: "80%",
         margin: 40,
       },
       [theme.breakpoints.down("md")]: {
@@ -49,24 +51,24 @@ const GbpUsd = ({ coin }) => {
       },
     },
     titleContainer: {
-      display: 'flex',
-      flexDirection: 'column',
-      width: '100%',
+      display: "flex",
+      flexDirection: "column",
+      width: "100%",
     },
     title: {
-      color: 'black',
-      fontFamily: 'antonio',
+      color: "black",
+      fontFamily: "antonio",
       fontWeight: 550,
       fontSize: 40,
       letterSpacing: 2,
       lineHeight: 1,
     },
     subtitle: {
-      fontFamily: 'garamond',
+      fontFamily: "garamond",
       lineHeight: 1,
       marginTop: 20,
       fontSize: 20,
-      color: '#444',
+      color: "#444",
     },
     forexContainer: {
       display: "flex",
@@ -126,10 +128,10 @@ const GbpUsd = ({ coin }) => {
         color: "black",
         backgroundColor: "rgba(255, 255, 255, 0.9)",
       },
-      [theme.breakpoints.down('lg')]: {
+      [theme.breakpoints.down("lg")]: {
         fontSize: 18,
         margin: 4,
-        '&:hover': {
+        "&:hover": {
           fontSize: 19,
         },
       },
@@ -164,19 +166,19 @@ const GbpUsd = ({ coin }) => {
       },
     },
     containerLabel: {
-      fontFamily: 'antonio',
+      fontFamily: "antonio",
       fontSize: 40,
       fontWeight: 550,
-      width: '90%',
+      width: "90%",
       lineHeight: 1.4,
-      [theme.breakpoints.down('md')]: {
+      [theme.breakpoints.down("md")]: {
         fontSize: 30,
       },
     },
     containerPrice: {
-      fontFamily: 'antonio',
+      fontFamily: "antonio",
       fontSize: 20,
-      width: '90%',
+      width: "90%",
       lineHeight: 1.4,
       color: "#000",
     },
@@ -202,75 +204,75 @@ const GbpUsd = ({ coin }) => {
         color: "#333",
       },
     },
-  }))
+  }));
 
-  const classes = useStyles()
+  const classes = useStyles();
 
   const darkTheme = createTheme({
     palette: {
       primary: {
-        main: '#fff',
+        main: "#fff",
       },
-      type: 'dark',
+      type: "dark",
     },
-  })
+  });
 
   function EpochToDate(epoch) {
-    if (epoch < 10000000000) epoch *= 1000 // convert to milliseconds (Epoch is usually expressed in seconds, but Javascript uses Milliseconds)
-    var epoch = epoch + new Date().getTimezoneOffset() * -1
-    var date = new Date(epoch) //for timeZone
-    var hours = date.getHours()
-    var minutes = date.getMinutes()
-    var seconds = date.getSeconds()
+    if (epoch < 10000000000) epoch *= 1000; // convert to milliseconds (Epoch is usually expressed in seconds, but Javascript uses Milliseconds)
+    var epoch = epoch + new Date().getTimezoneOffset() * -1;
+    var date = new Date(epoch); //for timeZone
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
     if (hours < 10) {
-      hours = '0' + hours
+      hours = "0" + hours;
     }
     if (minutes < 10) {
-      minutes = '0' + minutes
+      minutes = "0" + minutes;
     }
     if (seconds < 10) {
-      seconds = '0' + seconds
+      seconds = "0" + seconds;
     }
 
-    var time = hours + ':' + minutes + ':' + seconds
-    return time
+    var time = hours + ":" + minutes + ":" + seconds;
+    return time;
   }
 
   // current date to be displayed at tooltip
-  var today = new Date()
-  var year = today.getFullYear()
-  var month = today.getMonth()
-  var date = today.getDate()
-  var day = today.getDay()
-  const dayOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+  var today = new Date();
+  var year = today.getFullYear();
+  var month = today.getMonth();
+  var date = today.getDate();
+  var day = today.getDay();
+  const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   if (date < 10) {
-    date = '0' + date
+    date = "0" + date;
   }
   const monthOfYear = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec',
-  ]
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
   var formatDate =
-    dayOfWeek[day] + ' ' + date + ' ' + monthOfYear[month] + ' ' + year
+    dayOfWeek[day] + " " + date + " " + monthOfYear[month] + " " + year;
 
   // tick stream data status
-  const [price, setPrice] = useState([])
-  const [tick, setTick] = useState([])
-  const [ask, setAsk] = useState([])
-  const [bid, setBid] = useState([])
+  const [price, setPrice] = useState([]);
+  const [tick, setTick] = useState([]);
+  const [ask, setAsk] = useState([]);
+  const [bid, setBid] = useState([]);
 
   // Error message when market closed on weekends, or when Deriv API not working
-  const [marketStatus, setMarketStatus] = useState()
+  const [marketStatus, setMarketStatus] = useState();
 
   var lineChart = {
     labels: tick,
@@ -278,33 +280,39 @@ const GbpUsd = ({ coin }) => {
       {
         data: price,
         label: `Tick Stream for ${forex}`,
-        color: 'black',
-        borderColor: '#174f1a',
+        color: "black",
+        borderColor: "#174f1a",
       },
     ],
-  }
+  };
 
   // chart display configuration
   var chartOptions = {
     responsive: true,
     showLine: true,
     showtooltip: true,
-    animation: {
-      duration: 'speed * 1.5',
-      easing: 'linear',
+    animations: {
+      tension: { duration: "speed", easing: "linear" },
+      borderWidth: { duration: 3000, from: 1, to: 3 },
+      borderColor: {
+        type: "color",
+        duration: 3000,
+        from: "grey",
+        to: "#174f1a",
+      },
     },
     elements: {
       line: {
         tension: 0.5,
       },
       point: {
-        pointStyle: 'circular',
+        pointStyle: "circular",
         radius: 5,
         pointHoverRadius: 10,
       },
     },
     interaction: {
-      mode: 'nearest',
+      mode: "nearest",
       intersect: false,
     },
     scale: {
@@ -315,10 +323,10 @@ const GbpUsd = ({ coin }) => {
       y: {
         afterDataLimits(scale) {
           // add 5% to both ends of range
-          var range = scale.max - scale.min
-          var grace = range * 0.05
-          scale.max += grace
-          scale.min -= grace
+          var range = scale.max - scale.min;
+          var grace = range * 0.05;
+          scale.max += grace;
+          scale.min -= grace;
         },
       },
     },
@@ -330,65 +338,65 @@ const GbpUsd = ({ coin }) => {
         displayColors: false,
         callbacks: {
           title: function (context) {
-            return `${formatDate}, ${context[0].label}`
+            return `${formatDate}, ${context[0].label}`;
           },
           label: function (context) {
-            return `Price: ${context.parsed.y}`
+            return `Price: ${context.parsed.y}`;
           },
         },
       },
     },
-  }
+  };
 
   // receive data via websocket
-  var api_call = 'wss://ws.binaryws.com/websockets/v3?app_id=1089'
+  var api_call = "wss://ws.binaryws.com/websockets/v3?app_id=1089";
   useEffect(() => {
-    var ws = new WebSocket(api_call)
+    var ws = new WebSocket(api_call);
     ws.onopen = (evt) => {
       ws.send(
         JSON.stringify({
           ticks_history: forex,
           adjust_start_time: 1,
           count: 10,
-          end: 'latest',
+          end: "latest",
           start: 1,
-          style: 'ticks',
-        }),
-      )
-      ws.send(JSON.stringify({ ticks: forex }))
+          style: "ticks",
+        })
+      );
+      ws.send(JSON.stringify({ ticks: forex }));
       ws.send(
         JSON.stringify({
-          active_symbols: 'brief',
-        }),
-      )
-    }
+          active_symbols: "brief",
+        })
+      );
+    };
 
     ws.onmessage = function (evt) {
-      var data = JSON.parse(evt.data)
+      var data = JSON.parse(evt.data);
       if (data.history != null) {
-        var tickHist = data.history
-        setHistoricData(tickHist)
+        var tickHist = data.history;
+        setHistoricData(tickHist);
         for (let i = 0; i < 10; i++) {
           setTick((currentTick) => [
             ...currentTick,
             EpochToDate(tickHist.times[i]),
-          ])
+          ]);
         }
         // console.log("ticks: %o", tick);
-        setPrice(tickHist.prices)
-        setflag(true)
+        setPrice(tickHist.prices);
+        setflag(true);
       }
       if (data.tick != null) {
-        var tickInfo = data.tick
+        var tickInfo = data.tick;
         // console.log(tickInfo);
         if (tickInfo.quote != null && tickInfo.epoch != null) {
           setTick((currentTick) => [
             ...currentTick,
             EpochToDate(tickInfo.epoch),
-          ])
-          setPrice((currentPrice) => [...currentPrice, tickInfo.quote])
-          setAsk((currentAsk) => [...currentAsk, tickInfo.ask])
-          setBid((currentBid) => [...currentBid, tickInfo.bid])
+          ]);
+          setPrice((currentPrice) => [...currentPrice, tickInfo.quote]);
+          setAsk((currentAsk) => [...currentAsk, tickInfo.ask]);
+          setBid((currentBid) => [...currentBid, tickInfo.bid]);
         }
       }
       // when market close
@@ -397,9 +405,9 @@ const GbpUsd = ({ coin }) => {
           return (
             <Typography
               style={{
-                color: 'red',
-                fontStyle: 'italic',
-                marginLeft: 'auto',
+                color: "red",
+                fontStyle: "italic",
+                marginLeft: "auto",
                 fontWeight: 550,
                 lineHeight: 1.4,
                 padding: 5,
@@ -408,8 +416,8 @@ const GbpUsd = ({ coin }) => {
               Important notice! Market is closed at the moment. Data displayed
               is the last active market data before closing.
             </Typography>
-          )
-        })
+          );
+        });
       }
 
       // when market open but Deriv API not responding
@@ -418,9 +426,9 @@ const GbpUsd = ({ coin }) => {
           return (
             <Typography
               style={{
-                color: '#FF5F1F',
-                fontStyle: 'italic',
-                marginLeft: 'auto',
+                color: "#FF5F1F",
+                fontStyle: "italic",
+                marginLeft: "auto",
                 fontWeight: 550,
                 lineHeight: 1.4,
                 padding: 5,
@@ -429,52 +437,52 @@ const GbpUsd = ({ coin }) => {
               Slow data update due to server maintenance. Please come back in a
               while.
             </Typography>
-          )
-        })
+          );
+        });
       }
 
       // get market for scrollbar
-      var active_symbol = data.active_symbols
+      var active_symbol = data.active_symbols;
       if (active_symbol != null) {
         for (let marSym of active_symbol) {
-          if (marSym.symbol_type === 'forex') {
+          if (marSym.symbol_type === "forex") {
             setForexSymbols((currentSymbols) => [
               ...currentSymbols,
               marSym.symbol,
-            ])
+            ]);
             setDisplayNames((currentNames) => [
               ...currentNames,
               marSym.display_name,
-            ])
+            ]);
           }
         }
       }
-    }
+    };
 
-    return () => ws.close()
-  }, [])
+    return () => ws.close();
+  }, []);
 
   // array for dropdown
-  const [forexSymbols, setForexSymbols] = useState([])
-  const [displayNames, setDisplayNames] = useState([])
-  let displayName = ''
+  const [forexSymbols, setForexSymbols] = useState([]);
+  const [displayNames, setDisplayNames] = useState([]);
+  let displayName = "";
 
   const forexList = forexSymbols.map((forexSymbol, key) => {
     if (forexSymbol === forex) {
-      displayName = displayNames[key]
+      displayName = displayNames[key];
     }
     return (
       <div
         onClick={() => {
-          window.location.href = `/forex?forex=${forexSymbol}`
+          window.location.href = `/forex?forex=${forexSymbol}`;
         }}
         className={classes.forexItem}
         key={key}
       >
         {displayNames[key]}
       </div>
-    )
-  })
+    );
+  });
 
   const [openList, setOpenList] = useState(false);
   const [openColor, setOpenColour] = useState("#abb6ab");
@@ -515,13 +523,14 @@ const GbpUsd = ({ coin }) => {
               View All Forex Pairs &nbsp;&nbsp;
               <KeyboardDoubleArrowDownIcon className={classes.icon} />
             </div>
+
             {openList && (
-              <div className={classes.listContainer}>
+              <SlideDown className={classes.listContainer}>
                 <Typography className={classes.listDescription}>
                   Select a forex pair to view its real-time chart.
                 </Typography>
                 <div className={classes.forexList}>{forexList}</div>
-              </div>
+              </SlideDown>
             )}
 
             <div className={classes.textContainer}>
@@ -543,7 +552,7 @@ const GbpUsd = ({ coin }) => {
             <div className={classes.container}>
               {!historicData | (flag === false) ? (
                 <CircularProgress
-                  style={{ color: '#c6cec6' }}
+                  style={{ color: "#c6cec6" }}
                   size={250}
                   thickness={1}
                 />
@@ -557,7 +566,7 @@ const GbpUsd = ({ coin }) => {
         </div>
       </div>
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default GbpUsd
+export default GbpUsd;
