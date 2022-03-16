c59eab72cb56fd2e2c89f081a3bab765c5c888e1
Tatiana Cheah <tatiana.cheah@gmail.com>
2022-03-16 10:02:46 +0800

    comment out onchange

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,6 @@ import {
 } from "@material-ui/core";
 import { useNavigate } from "react-router-dom";
 import { FaRegCopyright } from "react-icons/fa";
-import { BottomNavigationAction } from "@mui/material";
 
 const useStyles = makeStyles((theme) => ({
   footerContainer: {
@@ -220,14 +219,14 @@ const Footer = () => {
                 <Typography
                   onClick={() => navigate("/help")}
                   className={classes.miniTitle}
-                  onChange={window.scroll({ top: 0, behavior: "smooth" })}
+                  // onChange={window.scroll({ top: 0, behavior: "smooth" })}
                 >
                   Contact Us
                 </Typography>
                 <Typography
                   onClick={() => navigate("/help")}
                   className={classes.miniTitle}
-                  onChange={window.scroll({ top: 0, behavior: "smooth" })}
+                  // onChange={window.scroll({ top: 0, behavior: "smooth" })}
                 >
                   Help
                 </Typography>
@@ -236,14 +235,14 @@ const Footer = () => {
                 <Typography
                   onClick={() => navigate("/crypto")}
                   className={classes.miniTitle}
-                  onChange={window.scroll({ top: 0, behavior: "smooth" })}
+                  // onChange={window.scroll({ top: 0, behavior: "smooth" })}
                 >
                   Search Cryptocurrency
                 </Typography>
                 <Typography
                   onClick={() => navigate("/")}
                   className={classes.miniTitle}
-                  onChange={window.scroll({ top: 0, behavior: "smooth" })}
+                  // onChange={window.scroll({ top: 0, behavior: "smooth" })}
                 >
                   Trending Markets
                 </Typography>
@@ -251,7 +250,7 @@ const Footer = () => {
                 <Typography
                   onClick={() => navigate("/landing")}
                   className={classes.miniTitle}
-                  onChange={window.scroll({ top: 0, behavior: "smooth" })}
+                  // onChange={window.scroll({ top: 0, behavior: "smooth" })}
                 >
                   Promotional
                 </Typography>
